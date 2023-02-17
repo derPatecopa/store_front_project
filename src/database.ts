821b7982f9053afc2f69d48bceba81102008bba8
derPatecopa <mb89@me.com>
2023-02-17 10:15:32 +0100

    added docker container made it run with database

--- src/database.ts
+++ src/database.ts
@@ -9,17 +9,33 @@ const {
   POSTGRES_DB,
   POSTGRES_TEST_DB,
   POSTGRES_USER,
+  POSTGRES_TEST_USER,
   POSTGRES_PASSWORD,
+  POSTGRES_TEST_PASSWORD,
   ENV,
 } = process.env;
 
 let client: Pool | null = null;
+console.log("This is the ENV variable: " + ENV);
 
-client = new Pool({
-  host: POSTGRES_HOST,
-  port: POSTGRES_PORT as unknown as number,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-});
-
+if (ENV === "dev") {
+  console.log("Connection regular database");
+  client = new Pool({
+    host: POSTGRES_HOST,
+    port: POSTGRES_PORT as unknown as number,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+if (ENV === "test") {
+  console.log("Connection test database");
+  client = new Pool({
+    host: POSTGRES_HOST,
+    port: POSTGRES_PORT as unknown as number,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_TEST_USER,
+    password: POSTGRES_TEST_PASSWORD,
+  });
+}
 export default client;
