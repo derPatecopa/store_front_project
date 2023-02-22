fcc8b5501a0d79690c447a9ddbd381ef90a8a643
derPatecopa <mb89@me.com>
2023-02-22 15:56:04 +0100

    added show test for ordermodel

--- src/tests/C_ordersmodelSpec.ts
+++ src/tests/C_ordersmodelSpec.ts
@@ -26,5 +26,22 @@ it("create method should add an order", async () => {
     expect(result.quantity).toEqual(testOrder.quantity);
     expect(result.user_id).toEqual(testOrder.user_id);
   });
+  it("index method should return a list of orders", async () => {
+    const result = await store.index();
+    expect(result.length).toBeGreaterThan(0);
+    expect(result[0].product_id).toEqual(testOrder.product_id);
+    expect(result[0].quantity).toEqual(testOrder.quantity);
+    expect(result[0].user_id).toEqual(testOrder.user_id);
+  });
+  it("show method should return the correct order", async () => {
+    const createdOrder = await store.create(testOrder);
+    if(!createdOrder.id) {
+      throw new Error("Failed to create order");
+    }
+    const result = await store.show(createdOrder.id.toString());
+    expect(result.product_id).toEqual(testOrder.product_id);
+    expect(result.quantity).toEqual(testOrder.quantity);
+    expect(result.user_id).toEqual(testOrder.user_id);
+  });
 });
 
