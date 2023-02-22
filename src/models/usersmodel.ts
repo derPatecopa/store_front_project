a8d0ecfd4afbc4ad0f9969abb0c01150904d8112
derPatecopa <mb89@me.com>
2023-02-22 10:15:57 +0100

    added authenticate and testing

--- src/models/usersmodel.ts
+++ src/models/usersmodel.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import Client from "../database";
 import dotenv from "dotenv";
+import { restart } from "nodemon";
 
 dotenv.config();
 
@@ -8,12 +9,12 @@ const saltRounds = process.env.SALT_ROUNDS as string;
 const pepper = process.env.BCRYPT_PASSWORD;
 
 export type User = {
-  id?: number,
+  id?: number;
   //designing the variables with _ because, in postgres there is no capital letter, makes it harder to test in jasmine
-  first_name: string,
-  last_name: string,
-  user_name: string,
-  password: string,
+  first_name: string;
+  last_name: string;
+  user_name: string;
+  password: string;
 };
 
 const defaultUser = {
@@ -32,13 +33,51 @@ export class UserStore {
         const sql =
           "INSERT INTO users (first_name, last_name, user_name, password) VALUES ($1, $2, $3, $4) RETURNING *";
         const hash = bcrypt.hashSync(u.password + pepper, parseInt(saltRounds));
-        const result = await conn.query(sql, [u.first_name, u.last_name, u.user_name, hash]);
+        const result = await conn.query(sql, [
+          u.first_name,
+          u.last_name,
+          u.user_name,
+          hash,
+        ]);
         const user = result.rows[0];
         conn.release();
         return user;
       } catch (err) {
+        throw new Error(`Could not add new user ${u.user_name}. Error: ${err}`);
+      }
+    } else {
+      console.log("Client is falsy");
+      return defaultUser;
+    }
+  }
+  async authenticate(
+    user_name: string,
+    password: string
+  ): Promise<User | null> {
+    if (Client) {
+      try {
+        const conn = await Client.connect();
+        const sql = "SELECT * FROM users WHERE user_name=($1)";
+      
+
+        const result = await conn.query(sql, [user_name]);
+        //console.log("This is result" + result.rows[0].user_name, result.rows[0].password);
+
+        //console.log("Password plus pepper" + password + pepper);
+        //checking if any rows are being returned
+        //console.log("This is length " + result.rows.length)
+        if (result.rows.length > 0) {
+          //console.log("Length is greater 0")
+          const user = result.rows[0];
+         
+          if (bcrypt.compareSync(password + pepper, user.password)){
+            return user;
+          }
+        }
+        return null;
+      } catch (err) {
         throw new Error(
-          `Could not add new user ${u.user_name}. Error: ${err}`
+          `Could not authenticate user ${user_name}. Error: ${err}`
         );
       }
     } else {
--- src/tests/usersmodelSpec.ts
+++ src/tests/usersmodelSpec.ts
@@ -1,4 +1,4 @@
-import {User, UserStore} from "../models/usersmodel";
+import { User, UserStore } from "../models/usersmodel";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 
@@ -8,30 +8,66 @@ const pepper = process.env.BCRYPT_PASSWORD;
 
 const store = new UserStore();
 
-fdescribe("Products Model", () => {
-    it("should have a create method",() => {
+describe("Products Model", () => {
+  it("should have a create method", () => {
     expect(store.create).toBeDefined();
-});
-    it("create method should add a user to the users table", async() => {
-        const user:User = {
-            first_name: "John",
-            last_name: "Smith",
-            user_name: "JohnSmith",
-            password: "password123",
-        };
-        const createdUser = await store.create(user);
+  });
+  it("create method should add a user to the users table", async () => {
+    const user: User = {
+      first_name: "John",
+      last_name: "Smith",
+      user_name: "JohnSmith",
+      password: "password123",
+    };
+    const createdUser = await store.create(user);
 
-        expect(createdUser.id).toBeGreaterThan(0);
-        expect(createdUser).toEqual({
-            id: jasmine.any(Number),
-            first_name: "John",
-            last_name: "Smith",
-            user_name: "JohnSmith",
-            password: jasmine.any(String),
-        })
-        //console.log(createdUser.password);
-        //comparing the created password with the user password if it was hashed correctly
-        //saltRounds not neccessary to add, because it is just neccessary during the hashing process
-        expect(bcrypt.compareSync(user.password + pepper, createdUser.password)).toBe(true);
-    })
+    expect(createdUser.id).toBeGreaterThan(0);
+    expect(createdUser).toEqual({
+      id: jasmine.any(Number),
+      first_name: "John",
+      last_name: "Smith",
+      user_name: "JohnSmith",
+      password: jasmine.any(String),
+    });
+    //console.log(createdUser.password);
+    //comparing the created password with the user password if it was hashed correctly
+    //saltRounds not neccessary to add, because it is just neccessary during the hashing process
+    expect(
+      bcrypt.compareSync(user.password + pepper, createdUser.password)
+    ).toBe(true);
+  });
+  it("should have a authenticate method", () => {
+    expect(store.authenticate).toBeDefined();
+  });
+  it("should return a user when provided with a valid user name and password", async () => {
+    const user: User = {
+      first_name: "Marlon",
+      last_name: "Brando",
+      user_name: "MarlonBrando",
+      password: "mafia",
+    };
+   
+    await store.create(user);
+    const authenticatedUser = await store.authenticate(
+      user.user_name,
+      user.password
+    );
+    expect(authenticatedUser).not.toBeNull();
+    // console.log(
+    //   `This is authenticated user from spec:
+    //   ${authenticatedUser?.id} 
+    // ${authenticatedUser?.first_name} 
+    // ${authenticatedUser?.last_name} 
+    // ${authenticatedUser?.user_name} 
+    // ${authenticatedUser?.password}`
+    // );
+    expect(authenticatedUser?.user_name).toEqual(user.user_name);
+  });
+  it("should return null when provided with an invalid user name and password", async () => {
+    const authenticatedUser = await store.authenticate(
+      "invaliduser",
+      "invalidpassword"
+    );
+    expect(authenticatedUser).toBeNull();
+  });
 });
