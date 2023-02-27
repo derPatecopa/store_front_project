00a5800a9b0d78c73aff734c95d8c57eb6b3096e
derPatecopa <mb89@me.com>
2023-02-27 10:00:06 +0100

    added order_products table

---
+++ migrations/sqls/20230227085812-order-products-up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE order_products (
+  id SERIAL PRIMARY KEY,
+  order_id INT REFERENCES orders(id),
+  product_id INT REFERENCES products(id),
+  quantity INT,
+  user_id INT REFERENCES users(id)
+);
